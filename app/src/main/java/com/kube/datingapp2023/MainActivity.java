ac5940c74703474d2941bb7d6e66555d337eaba6
Doğuş ipeksaç <dogusipeksac@ogoodigital.com>
2022-11-14 21:09:28 +0300

    first commit

---
+++ app/src/main/java/com/kube/datingapp2023/MainActivity.java
@@ -0,0 +1,19 @@
+package com.kube.datingapp2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.kube.datingapp2023.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        View view=binding.getRoot();
+        setContentView(view);
+    }
+}
